9f5b7416ef5cd576c11c904fc9e95263ab053e79
Nqobizitha30 <69073321+Nqobizitha30@users.noreply.github.com>
2021-08-19 17:48:03 +0200

    Add files via upload

--- public/admin/home/js/adminmain.js
+++ public/admin/home/js/adminmain.js
@@ -1,31 +1,34 @@
 window.addEventListener("load", async function () {
-  const data = await fetch("/tollbooth_current_admin");
-  const currentAdmin = await data.json();
-  displayName(currentAdmin.boothName);
-  displayHistory(currentAdmin.history);
+	const data = await fetch("/tollbooth_current_admin");
+	const currentAdmin = await data.json();
+
+	if (!currentAdmin) document.location.pathname = "/admin";
+
+	displayName(currentAdmin.boothName);
+	displayHistory(currentAdmin.history);
 });
 
 const displayName = (name) =>
-  (document.getElementById("booth-name").textContent = name);
+	(document.getElementById("booth-name").textContent = name);
 
 const displayHistory = async (history) => {
-  history.forEach((element, index) => {
-    const row = document.createElement("tr");
-    const countColumn = document.createElement("td");
-    const nameColumn = document.createElement("td");
-    const regColumn = document.createElement("td");
-    const amountColumn = document.createElement("td");
+	history.forEach((element, index) => {
+		const row = document.createElement("tr");
+		const countColumn = document.createElement("td");
+		const nameColumn = document.createElement("td");
+		const regColumn = document.createElement("td");
+		const amountColumn = document.createElement("td");
 
-    amountColumn.textContent = element.amount;
-    countColumn.textContent = index + 1;
-    nameColumn.textContent = element.user.name;
-    regColumn.textContent = element.user.regNumber;
+		amountColumn.textContent = element.amount;
+		countColumn.textContent = index + 1;
+		nameColumn.textContent = element.user.name;
+		regColumn.textContent = element.user.regNumber;
 
-    row.appendChild(countColumn);
-    row.appendChild(nameColumn);
-    row.appendChild(regColumn);
-    row.appendChild(amountColumn);
+		row.appendChild(countColumn);
+		row.appendChild(nameColumn);
+		row.appendChild(regColumn);
+		row.appendChild(amountColumn);
 
-    document.querySelector("tbody").appendChild(row);
-  });
+		document.querySelector("tbody").appendChild(row);
+	});
 };
