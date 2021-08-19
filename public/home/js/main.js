6b18d3bde77ceaade9916d72509fff07d0538c1a
Nqobizitha30 <69073321+Nqobizitha30@users.noreply.github.com>
2021-08-19 17:54:37 +0200

    Add files via upload

--- public/home/js/main.js
+++ public/home/js/main.js
@@ -1,37 +1,42 @@
 window.addEventListener("load", async function () {
-  const data = await fetch("/tollbooth_current_user");
-  const currentUser = await data.json();
-  display(currentUser);
-  displayActivity(currentUser.activity);
+	const data = await fetch("/tollbooth_current_user");
+	const currentUser = await data.json();
+
+	if (!currentUser) window.location.pathname = "/";
+
+	display(currentUser);
 });
 
 function display(currentUser) {
-  //display currentUser name
-  const nameContainer = document.querySelector("#name span");
-  nameContainer.textContent = currentUser.name;
-  //display currentUser reg number
-  const regContainer = document.querySelector("#reg span");
-  regContainer.textContent = currentUser.regNumber;
-  //display currentUser balance
-  const balanceContainer = document.querySelector("#balance span");
-  balanceContainer.textContent = currentUser.balance;
+	//display currentUser name
+	const nameContainer = document.querySelector("#name span");
+	nameContainer.textContent = currentUser.name;
+	document.title = `${currentUser.name} Tollbooth Management System`;
+	//display currentUser reg number
+	const regContainer = document.querySelector("#reg span");
+	regContainer.textContent = currentUser.regNumber;
+	//display currentUser balance
+	const balanceContainer = document.querySelector("#balance span");
+	balanceContainer.textContent = currentUser.balance;
+	//display user activity
+	displayActivity(currentUser.activity);
 }
 
 function displayActivity(activities) {
-  //make table row
-  for (let activity of activities) {
-    const tableRow = document.createElement("tr");
-    const boothColumn = document.createElement("td");
-    const amaountColumn = document.createElement("td");
+	//make table row
+	for (let activity of activities) {
+		const tableRow = document.createElement("tr");
+		const boothColumn = document.createElement("td");
+		const amaountColumn = document.createElement("td");
 
-    //input data
-    boothColumn.textContent = activity.boothName;
-    amaountColumn.textContent = activity.amount;
-    //linking td-tr
-    tableRow.appendChild(boothColumn);
-    tableRow.appendChild(amaountColumn);
-    //linking table row to html table
-    const table = document.querySelector(".activity table");
-    table.appendChild(tableRow);
-  }
+		//input data
+		boothColumn.textContent = activity.boothName;
+		amaountColumn.textContent = activity.amount;
+		//linking td-tr
+		tableRow.appendChild(boothColumn);
+		tableRow.appendChild(amaountColumn);
+		//linking table row to html table
+		const table = document.querySelector(".activity table");
+		table.appendChild(tableRow);
+	}
 }
