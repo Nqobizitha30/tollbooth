b6d99298b883d743dd891b767f663be1ce7353e4
Nqobizitha30 <69073321+Nqobizitha30@users.noreply.github.com>
2021-08-19 17:45:45 +0200

    Add files via upload

--- model/user.model.js
+++ model/user.model.js
@@ -1,16 +1,17 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    userPhone: { type: String, required: true },
-    password: { type: String, required: true },
-    regNumber: { type: String, required: true },
-    balance: { type: Number, required: true },
-    activity: { type: Array, required: true },
-  },
-  { timestamps: true }
+	{
+		_id: { type: String },
+		name: { type: String, required: true },
+		email: { type: String, required: true },
+		userPhone: { type: String, required: true },
+		password: { type: String, required: true },
+		regNumber: { type: String, required: true },
+		balance: { type: Number, required: true },
+		activity: { type: Array, required: true },
+	},
+	{ timestamps: true }
 );
 
 const User = model("User", userSchema);
